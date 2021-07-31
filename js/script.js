90b3799270aa3f104212a4949769437b504f90f1
MarcusReitzl <mareitzl@edu.aau.at>
2021-07-31 02:20:40 +0200

    added section stats

--- js/script.js
+++ js/script.js
@@ -39,7 +39,8 @@ $(document).ready(function() {
 
 
     var skillsTopOffset = $('.skillsSection').offset().top;
-    console.log(skillsTopOffset);
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
 
     $(window).scroll(function() {
         if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
@@ -57,5 +58,17 @@ $(document).ready(function() {
             });  
         }
     });
+
+    if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+        $(".counter").each(function() {
+            var element = $(this);
+            var endVal = parseInt(element.text());
+
+            element.countUp(endVal);
+        })
+
+        countUpFinished = true;
+
+    }
 
 });
